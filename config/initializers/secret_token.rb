e5f2fb933387a6654d335fd86e61fa096af2fd29
Arsalan Ahmed <itsarsalan@yahoo.com>
2014-03-10 13:25:59 -0400

    Brand new Rails 4 application for ticket tracking

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticketracker::Application.config.secret_key_base = 'a9130ec9d224657d690c9f401dfaba9d4611d76f09478656066ef36f008c70d56b93f20dc0e655560f984e06c590760812419983d079a6fafe87f09cf084f038'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ticketracker::Application.config.session_store :cookie_store, key: '_ticketracker_session'
