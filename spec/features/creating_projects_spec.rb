0ff7de4c9d7f73b8db7ae5938af639bf494e93e6
Arsalan Ahmed <itsarsalan@yahoo.com>
2014-03-12 11:43:54 -0400

    Added validation to ensure names are specified when creating projects

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
-	scenario "can create a project" do
+	before do
 		visit '/'
-
 		click_link 'New Project'
+	end
 
+	scenario "can create a project" do
 		fill_in 'Name', with: 'Ticket Tracker'
 		fill_in 'Description', with: 'Ticket tracking'
 		click_button 'Create Project'
@@ -15,5 +16,13 @@ feature 'Creating Projects' do
 		project = Project.where(name: "Ticket Tracker").first
 		expect(page.current_url).to eql(project_url(project))
 		expect(page).to have_title("Ticket Tracker - Projects - Ticketracker")
+	end
+
+	scenario "cannot create a project without a name" do
+		fill_in 'Description', with: 'Ticket tracking'
+		click_button 'Create Project'
+ 
+ 		expect(page).to have_content('Project has not been created')
+ 		expect(page).to have_content("Name can't be blank")
 	end
 end
