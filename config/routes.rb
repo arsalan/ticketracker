aaa894dd335f6347b093bec7f4fb1522f868ca6c
Arsalan Ahmed <itsarsalan@yahoo.com>
2014-03-20 12:18:42 -0400

    Implemented creation of tickets for a project

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Ticketracker::Application.routes.draw do
-  resources :models
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'projects#index'
-  resources :projects
+  resources :projects do
+    resources :tickets
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
